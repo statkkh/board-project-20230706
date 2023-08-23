ed576da4b0d338a223c3f3b214f1a4f9b5ee061f
devjiraynor <jiraynor@naver.com>
2023-08-23 12:50:44 +0900

    2023.08.23. 12:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './style.css';
 
-export default function index() {
+//          component: Input 상자 컴포넌트          //
+export default function InputBox() {
+
+  //          render: Input 상자 렌더링         //
   return (
-    <div>index</div>
+    <div className='inputbox'>
+      <div className='inputbox-label'>주소*</div>
+      <div className='inputbox-container'>
+        <input className='input' placeholder='우편번호 찾기' />
+        <div className='icon-button'>
+          <div className='right-arrow-icon'></div>
+        </div>
+      </div>
+      <div className='inputbox-message'>우편번호를 선택해주세요.</div>
+    </div>
   )
 }
