ffa9b560550c2c46680dc684456563b032e5364d
khkim <stakkh4@gmail.com>
2023-09-08 21:57:03 +0900

    20230908

---
+++ src/mocks/favorite-list.mock.ts
@@ -0,0 +1,42 @@
+import FavoriteItem from "types/favorite-item.interface";
+
+
+const favoriteListMock: FavoriteItem[] = [
+    {
+        email: 'email@email.com',
+        nickname: '나는daniel',
+        profileImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_s86T3wmViKv6ajCRzCRgrjJ7_1yvRjFouQ&usqp=CAU',
+    },
+    {
+        email: 'email@email.com',
+        nickname: '나는daniel',
+        profileImage: null,
+    },
+    {
+        email: 'email@email.com',
+        nickname: '나는주코야키',
+        profileImage: null,
+    },
+    {
+        email: 'email@email.com',
+        nickname: '나는주코야키',
+        profileImage: null,
+    },
+    {
+        email: 'email@email.com',
+        nickname: '나는주코야키',
+        profileImage: null,
+    },
+    {
+        email: 'email@email.com',
+        nickname: '나는주코야키',
+        profileImage: null,
+    },
+    {
+        email: 'email@email.com',
+        nickname: '나는주코야키',
+        profileImage: null,
+    },
+];
+
+export default favoriteListMock;
