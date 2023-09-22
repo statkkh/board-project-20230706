cfa5623135d6a4b9a3387dac649befa000eaf16c
khkim <stakkh4@gmail.com>
2023-09-22 15:23:36 +0900

    board.store.ts change

--- src/stores/board.store.ts
+++ src/stores/board.store.ts
@@ -7,7 +7,7 @@ interface BoardStore {
 
     setTitle: (title: string) => void;
     setContents: (contents: string) => void;
-    setImage: (image: File | null) => void;
+    setImages: (images: File[]) => void;
 
     resetBoard: () => void;
 }
@@ -19,7 +19,7 @@ const useBoardStore = create<BoardStore>((set) => ({
 
     setTitle: (title: string) => {set((state) => ({ ...state, title }))},
     setContents: (contents: string) => {set((state) => ({ ...state, contents }))},
-    setImage: (image: File | null) => {set((state) => ({ ...state, image }))},
+    setImages: (image: File[]) => {set((state) => ({ ...state, image }))},
     // 게시물 작성중 다른 페이지 이동후 다시 왔을 때 reset 
     resetBoard: () => {set((state) => ({ ...state, title: '', contents: '', images: [] }))}
 }));
--- src/views/Board/Write/index.tsx
+++ src/views/Board/Write/index.tsx
@@ -1,7 +1,49 @@
 import React, { ChangeEvent, useRef, useState, useEffect }  from 'react';
 import './style.css';
+import { useBoardStore } from 'stores';
 
 export default function BoardWrite() {
+
+
+    //          state: 이미지 인풋 ref 상태          //
+    const imageInputRef = useRef<HTMLInputElement | null>(null);
+    //          state: 본문 텍스트 영역 ref 상태          //
+    const contentsTextAreaRef = useRef<HTMLTextAreaElement | null>(null);
+    //          state: 게시물 상태          //
+    const { title, setTitle } = useBoardStore();
+    const { contents, setContents } = useBoardStore();
+    const { images, setImages, resetBoard } = useBoardStore();
+    //          state: 게시물 이미지 URL 상태          //
+    const [imageUrls, setImageUrls] = useState<string[]>([]);
+  
+    
+    //          event handler: 제목 변경 이벤트 처리          //
+    const onTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const title = event.target.value;
+      setTitle(title);
+    }
+    //          event handler: 내용 변경 이벤트 처리          //
+    const onContentsChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+      const contents = event.target.value;
+      setContents(contents);
+      if (!contentsTextAreaRef.current) return;
+      contentsTextAreaRef.current.style.height = 'auto';
+      contentsTextAreaRef.current.style.height = `${contentsTextAreaRef.current.scrollHeight}px`;
+    }
+
+    //          event handler: 이미지 변경 이벤트 처리          //
+    const onImageChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      if (!event.target.files || !event.target.files.length) return;
+      const file = event.target.files[0];
+      const imageUrl = URL.createObjectURL(file);
+      const newImageUrls = imageUrls.map(url => url);
+      newImageUrls.push(imageUrl);
+      const newImages = images.map(image => image);
+      newImages.push(file);
+
+      setImageUrls(newImageUrls);
+      setImages(newImages);
+    }      
   return (
     <div>BoardWrite</div>
   )
