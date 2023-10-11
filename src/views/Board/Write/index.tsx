0974928dfe8c4d1c3f979095a034023dd81324bc
khkim <stakkh4@gmail.com>
2023-10-11 14:55:59 +0900

    이미지 관한 이벤트 처리

--- src/views/Board/Write/index.tsx
+++ src/views/Board/Write/index.tsx
@@ -15,7 +15,7 @@ export default function BoardWrite() {
     const { images, setImages, resetBoard } = useBoardStore();
     //          state: 게시물 이미지 URL 상태          //
     const [imageUrls, setImageUrls] = useState<string[]>([]);
-  
+
     //          event handler: 제목 변경 이벤트 처리          //
     const onTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const title = event.target.value;
@@ -36,8 +36,11 @@ export default function BoardWrite() {
       const imageUrl = URL.createObjectURL(file);
       const newImageUrls = imageUrls.map(url => url);
       newImageUrls.push(imageUrl);
+
       const newImages = images.map(image => image);
       newImages.push(file);
+
+      // console.log(newImages);
 
       setImageUrls(newImageUrls);
       setImages(newImages);
