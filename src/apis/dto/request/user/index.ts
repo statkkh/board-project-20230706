fb69d8f9468136acfb36d23339e1abe888dca152
khkim <stakkh4@gmail.com>
2023-10-11 09:53:17 +0900

    PatchProfileImageRequestDto

--- src/apis/dto/request/user/index.ts
+++ src/apis/dto/request/user/index.ts
@@ -1,5 +1,7 @@
 import PatchNicknameRequestDto from "./patch-nickname.request.dto";
+import PatchProfileImageRequestDto from "./patch-profileImage.request.dto";
 
 export type{
-    PatchNicknameRequestDto
+    PatchNicknameRequestDto,
+    PatchProfileImageRequestDto
 }    
