01cd612d7cb47841e63492eed2892a10d0d6f77c
khkim <stakkh4@gmail.com>
2023-10-11 09:12:14 +0900

    SignInResponseDto

--- src/apis/dto/response/auth/index.ts
+++ src/apis/dto/response/auth/index.ts
@@ -1,5 +1,5 @@
-import SignUpResponseDto from "./sign-up-response.dto";
-import SignInResponseDto from "./sign-in-response.dto";
+import SignUpResponseDto from "./sign-up.response.dto";
+import SignInResponseDto from "./sign-in.response.dto";
 
 export type{
     SignUpResponseDto,
