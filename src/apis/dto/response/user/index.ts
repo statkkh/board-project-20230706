7165d976fea189e5a80a26b4588bcb81bf21aef1
khkim <stakkh4@gmail.com>
2023-10-11 09:54:17 +0900

    PatchProfileImageResponseDto

--- src/apis/dto/response/user/index.ts
+++ src/apis/dto/response/user/index.ts
@@ -1,9 +1,11 @@
 import GetSignInUserResponseDto from "./get-sign-in-user.response.dto";
 import GetUserReponseDto from "./get-user.response.dto";
 import PatchNicknameResponseDto from "./patch-nickname.response.dto";
+import PatchProfileImageResponseDto from "./patch-profileImage.response.dto";
  
 export type{
-    GetSignInUserResponseDto,
     GetUserReponseDto,
-    PatchNicknameResponseDto
+    PatchNicknameResponseDto,
+    GetSignInUserResponseDto,
+    PatchProfileImageResponseDto
 }
